fa9b3820a41fe9968ae72f7b58cb6b41abc61ec8
phitheone <102269729+phitheone@users.noreply.github.com>
2022-09-20 17:12:14 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import banner from "./images/bnr.png";
+import catdc from "./images/catdiscord.png";
+import cattw from "./images/cattwitter.png";
+import catlmntf from "./images/catLMNFT.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="BannerContainer">
+        <img src={banner} />
+      </div>
+      <div className="CatsContainer">
+        <a href="https://twitter.com/Dumbass_cats" target="_blank">
+          <img className="CatT" src={cattw} />
         </a>
-      </header>
+        <a href="https://discord.gg/JYwJsTTpjQ" target="_blank">
+          <img className="CatD" src={catdc} />
+        </a>
+        <a href="/" target="_blank">
+          <img className="CatL" src={catlmntf} />
+        </a>
+      </div>
     </div>
   );
 }
